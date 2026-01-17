3ba817431515d4ddeff9746e7552eacfe1941bc8
NexusKorthos <korthos@wp.pl>
2026-01-17 18:32:04 +0100

    Time: 62 ms (85.12%) | Memory: 59.3 MB (80.17%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
@@ -2,38 +2,30 @@ class Solution {
 public:
     long long largestSquareArea(vector<vector<int>>& bottomLeft, vector<vector<int>>& topRight) {
 
+        int rectangles = bottomLeft.size();
         long long max_side = 0;
 
-        for (int a = 0; a < bottomLeft.size(); a++) {
+        for (int a = 0; a < rectangles; a++) {
 
             vector<int>& a_bl_cords = bottomLeft[a];
             vector<int>& a_tr_cords = topRight[a];
 
-            for (int b = 0; b < bottomLeft.size(); b++) {
-
-                if (a == b)
-                    continue;
+            for (int b = a + 1; b < rectangles; b++) {
 
                 vector<int>& b_bl_cords = bottomLeft[b];
                 vector<int>& b_tr_cords = topRight[b];
 
-                if (    b_bl_cords[0] < a_tr_cords[0] && b_bl_cords[1] < a_tr_cords[1] 
-                &&      a_bl_cords[0] < b_tr_cords[0] && a_bl_cords[1] < b_tr_cords[1]) {
+                int bl_x = max(a_bl_cords[0], b_bl_cords[0]);
+                int bl_y = max(a_bl_cords[1], b_bl_cords[1]);
 
-                    int bl_x = max(a_bl_cords[0], b_bl_cords[0]);
-                    int bl_y = max(a_bl_cords[1], b_bl_cords[1]);
+                int tr_x = min(a_tr_cords[0], b_tr_cords[0]);
+                int tr_y = min(a_tr_cords[1], b_tr_cords[1]);
 
-                    int tr_x = min(a_tr_cords[0], b_tr_cords[0]);
-                    int tr_y = min(a_tr_cords[1], b_tr_cords[1]);
-
-                    max_side = max(max_side, (long long)min(tr_x - bl_x, tr_y - bl_y));
-
-                }
+                max_side = max(max_side, (long long)min(tr_x - bl_x, tr_y - bl_y));
 
             }
 
         }
-
 
         return max_side * max_side;
         
